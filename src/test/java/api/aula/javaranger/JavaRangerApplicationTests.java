41d86cbdc844109d5801c7470e57e26306c189dd
Lucas Azevedo <lucas.azevedo@find-u.com.br>
2024-02-21 19:49:03 -0300

    first commit

---
+++ src/test/java/api/aula/javaranger/JavaRangerApplicationTests.java
@@ -0,0 +1,15 @@
+package api.aula.javaranger;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootTest
+class JavaRangerApplicationTests {
+}
